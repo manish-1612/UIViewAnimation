45a2ef99722b09974824bd41d26a84f38e0cd3ad
Manish Kumar <manish.kumar@innofied.com>
2015-09-17 19:34:43 +0530

    working on text draw from view

--- ViewAnimationPractice/ViewController.swift
+++ ViewAnimationPractice/ViewController.swift
@@ -25,7 +25,8 @@ class ViewController: UIViewController {
         
         //animateUILabel()
         //draw8WithAnimation()
-        animateViewWithUIBezierPath()
+        //animateViewWithUIBezierPath()
+        makeAnimatingText()
         
     }
     
@@ -112,7 +113,6 @@ class ViewController: UIViewController {
         pathLayer.lineJoin = kCALineJoinBevel
         view.layer.addSublayer(pathLayer)
 
-        
         var pathAnimationStart = CABasicAnimation(keyPath: "strokeStart")
         pathAnimationStart.toValue = 0.8
 
@@ -130,6 +130,115 @@ class ViewController: UIViewController {
     
     func makeAnimatingText(){
         
+        
+        var baseView = UIView(frame: CGRect(x: 0.0, y: 250.0, width:self.view.frame.size.width, height: 1.0))
+        baseView.backgroundColor = UIColor.redColor()
+        //self.view.addSubview(baseView)
+        
+        var baseView2 = UIView(frame: CGRect(x: 0.0, y: 235.0, width:self.view.frame.size.width, height: 1.0))
+        baseView2.backgroundColor = UIColor.redColor()
+        //self.view.addSubview(baseView2)
+
+        
+        var path = UIBezierPath()
+        // ----------------------------------------------------------------
+        //  drawing path for MANISH
+        // ----------------------------------------------------------------
+        
+        //path for M
+        path.moveToPoint(CGPoint(x: 25.0, y: 250.0))
+        path.addLineToPoint(CGPoint(x: 35.0, y: 210.0))
+        path.addLineToPoint(CGPoint(x: 42.0, y: 235.0))
+        path.addLineToPoint(CGPoint(x: 55.0, y: 210.0))
+        path.addLineToPoint(CGPoint(x: 45.0, y: 248.0))
+        
+        path.addCurveToPoint(CGPoint(x: 50.0, y: 248.0), controlPoint1: CGPoint(x: 47.0, y: 250.0), controlPoint2: CGPoint(x: 48.0, y: 250.0))
+        
+        
+        //path for a
+        path.addLineToPoint(CGPoint(x: 60.0, y: 235.0))
+        path.addCurveToPoint(CGPoint(x: 68.0, y: 235.0), controlPoint1: CGPoint(x: 62.50, y: 232.0), controlPoint2: CGPoint(x: 65.5, y: 232.0))
+        path.moveToPoint(CGPoint(x: 54.0, y: 243.0))
+        path.addCurveToPoint(CGPoint(x: 68.0, y: 235.0), controlPoint1: CGPoint(x: 56.5, y: 252.0), controlPoint2: CGPoint(x: 66.0, y: 252.0))
+        path.moveToPoint(CGPoint(x: 66.0, y: 244.0))
+        path.addCurveToPoint(CGPoint(x: 73.0, y: 245.0), controlPoint1: CGPoint(x: 67.0, y: 250.0), controlPoint2: CGPoint(x: 70.0, y: 250.0))
+        
+        //path for n
+        path.addLineToPoint(CGPoint(x: 80.0, y: 237.0))
+        path.addCurveToPoint(CGPoint(x: 85.0, y: 235.0), controlPoint1: CGPoint(x: 83.0, y: 232.0), controlPoint2: CGPoint(x: 84.0, y: 232.0))
+        path.addLineToPoint(CGPoint(x: 80.0, y: 250.0))
+        path.moveToPoint(CGPoint(x: 84.0, y: 240.0))
+        path.addCurveToPoint(CGPoint(x: 94.0, y: 240.0), controlPoint1: CGPoint(x: 90.0, y: 232.0), controlPoint2: CGPoint(x: 93.0, y: 232.0))
+        path.addLineToPoint(CGPoint(x: 91.0, y: 248.0))
+        path.addCurveToPoint(CGPoint(x: 97.0, y: 248.0), controlPoint1: CGPoint(x: 92.0, y: 250.0), controlPoint2: CGPoint(x: 96.0, y: 250.0))
+        
+        //path for i
+        path.addLineToPoint(CGPoint(x: 101.0, y: 236.0))
+        path.moveToPoint(CGPoint(x: 101.0, y: 234.0))
+        path.addLineToPoint(CGPoint(x: 103.0, y: 232.0))
+        path.moveToPoint(CGPoint(x: 97.0, y: 248.0))
+        path.addCurveToPoint(CGPoint(x: 102.0, y: 248.0), controlPoint1: CGPoint(x: 98.0, y: 250.0), controlPoint2: CGPoint(x: 101.0, y: 250.0))
+
+        
+        //path for s
+        path.addLineToPoint(CGPoint(x: 115.0, y: 235.0))
+        path.addCurveToPoint(CGPoint(x: 110.0, y: 235.0), controlPoint1: CGPoint(x: 114.5, y: 232.0), controlPoint2: CGPoint(x: 110.5, y: 232.0))
+        path.addLineToPoint(CGPoint(x: 115.0, y: 245.0))
+        path.addCurveToPoint(CGPoint(x: 106.0, y: 247.0), controlPoint1: CGPoint(x: 115.0, y: 246.0), controlPoint2: CGPoint(x: 110.0, y: 255.0))
+        path.addLineToPoint(CGPoint(x: 103.0, y: 242.0))
+        
+        
+        //path for h
+        path.moveToPoint(CGPoint(x: 110.0, y: 250.0))
+        path.addCurveToPoint(CGPoint(x: 119.0, y: 245.0), controlPoint1: CGPoint(x: 113.0, y: 250.0), controlPoint2: CGPoint(x: 117.0, y: 246.20))
+        path.addCurveToPoint(CGPoint(x: 129.0, y: 213.0), controlPoint1: CGPoint(x: 124.0, y: 235.0), controlPoint2: CGPoint(x: 129.0, y: 225.0))
+        path.addCurveToPoint(CGPoint(x: 127.0, y: 213.0), controlPoint1: CGPoint(x: 128.0, y: 210.0), controlPoint2: CGPoint(x: 128.0, y: 210.0))
+        path.addLineToPoint(CGPoint(x: 117.0, y: 250.0))
+        path.addCurveToPoint(CGPoint(x: 130.0, y: 247.0), controlPoint1: CGPoint(x: 122.0, y: 232.0), controlPoint2: CGPoint(x: 135.0, y: 232.0))
+        path.addCurveToPoint(CGPoint(x: 135.0, y: 248.0), controlPoint1: CGPoint(x: 131.5, y: 250.0), controlPoint2: CGPoint(x: 133.5, y: 250.0))
+        
+        
+        var pathLayer = CAShapeLayer()
+        pathLayer.frame = view.bounds
+        pathLayer.path = path.CGPath
+        pathLayer.strokeColor = UIColor.blueColor().CGColor
+        pathLayer.fillColor = nil
+        pathLayer.lineWidth = 1.0
+        pathLayer.strokeStart = 0.0
+        pathLayer.strokeEnd = 0.1
+        pathLayer.lineJoin = kCALineJoinBevel
+        view.layer.addSublayer(pathLayer)
+        
+        
+        var pathAnimationStart = CABasicAnimation(keyPath: "strokeStart")
+        pathAnimationStart.toValue = 0.7
+        
+        var pathAnimationStop = CABasicAnimation(keyPath: "strokeEnd")
+        pathAnimationStop.toValue = 1.0
+        
+        var animationGroup = CAAnimationGroup()
+        animationGroup.animations = [pathAnimationStart , pathAnimationStop]
+        animationGroup.duration = 5.0
+        animationGroup.repeatCount = HUGE
+        animationGroup.autoreverses = true
+        pathLayer.addAnimation(animationGroup, forKey: "animateView")
+        
+        
+//        var pathLayer = CAShapeLayer()
+//        pathLayer.frame = view.bounds
+//        pathLayer.path = path.CGPath
+//        pathLayer.strokeColor = UIColor.blueColor().CGColor
+//        pathLayer.fillColor = nil
+//        pathLayer.lineWidth = 1.0
+//        pathLayer.lineJoin = kCALineJoinBevel
+//        view.layer.addSublayer(pathLayer)
+//        
+//        
+//        var pathAnimation = CABasicAnimation(keyPath: "strokeEnd")
+//        pathAnimation.duration = 2.0
+//        pathAnimation.fromValue = 0.0
+//        pathAnimation.toValue = 1.0
+//        pathLayer.addAnimation(pathAnimation, forKey: "strokeEnd")
     }
     
     
